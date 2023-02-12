5b6693cf9e0db6cb48cd3081e707fc8edbdc9d80
Hamid Farmani <hamidfarmani1@gmail.com>
2023-02-12 14:20:20 +0100

    feat: set up RabbitMQ module producer and configuration
    - Add a RabbitMQ module to handle the messaging and communication between different parts of the system.
    - Set up the RabbitMQ producer in the module to send messages to the RabbitMQ broker.
    - Configure the RabbitMQ producer with the appropriate settings such as the exchange type, routing key, and queue name.

--- notification/src/main/java/com/hamid/notification/NotificationApplication.java
+++ notification/src/main/java/com/hamid/notification/NotificationApplication.java
@@ -1,12 +1,28 @@
 package com.hamid.notification;
 
+import com.hamid.amqp.RabbitMQMessageProducer;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.hamid.notification", "com.hamid.amqp"})
 public class NotificationApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(NotificationApplication.class, args);
   }
+
+//  @Bean
+//  CommandLineRunner commandLineRunner(
+//      RabbitMQMessageProducer producer, NotificationConfig notificationConfig) {
+//    return args -> {
+//      producer.publish(
+//          new Person("Hamid", 28),
+//          notificationConfig.getInternalExchange(),
+//          notificationConfig.getInternalNotificationRoutingKey());
+//    };
+//  }
+//
+//  record Person(String name, int age){}
 }
